c14e8d463ba8fcf7f23e6a5ed33cdd201a7d0883
alwaysCwC <always0537@gmail.com>
2023-03-20 16:57:15 +0800

    更新

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	//@ts-ignore
+	import Header from "./Header.svelte";
+</script>
+<div>
+	<header>
+		<Header />
+	</header>
+
+	<main>
+		<div class="container body-content my-2">
+			<slot />
+		</div>
+	</main>
+
+	<footer class="my-2">
+		<div class="text-center">
+			<p>
+				version 0.0.1
+			</p>
+		</div>
+	</footer>
+</div>
