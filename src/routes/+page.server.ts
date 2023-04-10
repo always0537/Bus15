a2ead697cbb3213c82f021418cc1cbd238498928
Peace_Wang <Peace_Wang@wiwynn.com>
2023-04-10 08:44:57 +0800

    login link adjust

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import { setContext } from 'svelte';
 
 const lineLoginLink: string = (`https://access.line.me/oauth2/v2.1/authorize?response_type=code
 &client_id=${env.LINE_client_id}
-&redirect_uri=${env.LINE_redirect_uri}
+&redirect_uri=${env.LINE_redirect_uri}/login
 &state=login
 &scope=openid%20profile`);
 
