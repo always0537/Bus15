8ff56d4b0522bc79ef61b709ab150097ffdddc44
Peace Wang 王詠平 <pwang@continental-consultingltd.com>
2023-04-10 22:50:59 +0800

    change jwt sign

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -37,13 +37,13 @@ export const GET = async ({url}): Promise<Response> => {
             const checkBusMember = await isBusMember(lineIdToken.name);
             if (checkBusMember.result) {
                 await addNewUser(newBusUser);
-                token = jwt.sign(JSON.stringify(newBusUser), env.JWT_secret, {expiresIn: '7d'});
+                token = jwt.sign(newBusUser, env.JWT_secret, {expiresIn: '7d'});
             }
             result = (checkBusMember.result) ? { status: 200, message: "註冊成功", user: newBusUser, token: token }
                 : { status: 400, message: "非巴士團成員", user: null, token: null};
         }
         else {
-            token = jwt.sign(JSON.stringify(user), env.JWT_secret, {expiresIn: '7d'});
+            token = jwt.sign(user, env.JWT_secret, {expiresIn: '7d'});
             result = { status: 200, message: "登入成功", user: user, token: token };
         }
     }
@@ -56,40 +56,40 @@ export const GET = async ({url}): Promise<Response> => {
     }
 };
 
-export const POST = async ({request}): Promise<Response> => {
-    const lineIdToken = await request.json();
-    if(lineIdToken.sub == null) return new Response(JSON.stringify({status: 400, message: "無效的LineIdToken"}), { status: 400 });
-    try {        
-        //check if user exists
-        const user = await isUserRegistered(lineIdToken.sub);
-        if (user == null) {
-            let newBusUser: User = {
-                _id: lineIdToken.sub,
-                lineName: lineIdToken.name,
-                picture: lineIdToken.picture,
-                realName: "",
-                lastDraw: 0,
-                formatLastDraw: 0,
-                leftDrawTimes: 0
-            }
-            //check if user is bus member
-            const checkBusMember = await isBusMember(lineIdToken.name);
-            if (checkBusMember.result) await addNewUser(newBusUser);
-            result = (checkBusMember.result) ? { status: 200, message: "註冊成功", user: newBusUser }
-                : { status: 400, message: "非巴士團成員", user: null };
-        }
-        else {
-            result = { status: 200, message: "登入成功", user: user };
-        }
-    }
-    catch (err) {
-        console.log(err);
-        result = { status: 500, message: "伺服器錯誤", user: null };
-    }
-    finally{
-        return new Response(JSON.stringify(result), { status: result.status });
-    }
-};
+// export const POST = async ({request}): Promise<Response> => {
+//     const lineIdToken = await request.json();
+//     if(lineIdToken.sub == null) return new Response(JSON.stringify({status: 400, message: "無效的LineIdToken"}), { status: 400 });
+//     try {        
+//         //check if user exists
+//         const user = await isUserRegistered(lineIdToken.sub);
+//         if (user == null) {
+//             let newBusUser: User = {
+//                 _id: lineIdToken.sub,
+//                 lineName: lineIdToken.name,
+//                 picture: lineIdToken.picture,
+//                 realName: "",
+//                 lastDraw: 0,
+//                 formatLastDraw: 0,
+//                 leftDrawTimes: 0
+//             }
+//             //check if user is bus member
+//             const checkBusMember = await isBusMember(lineIdToken.name);
+//             if (checkBusMember.result) await addNewUser(newBusUser);
+//             result = (checkBusMember.result) ? { status: 200, message: "註冊成功", user: newBusUser }
+//                 : { status: 400, message: "非巴士團成員", user: null };
+//         }
+//         else {
+//             result = { status: 200, message: "登入成功", user: user };
+//         }
+//     }
+//     catch (err) {
+//         console.log(err);
+//         result = { status: 500, message: "伺服器錯誤", user: null };
+//     }
+//     finally{
+//         return new Response(JSON.stringify(result), { status: result.status });
+//     }
+// };
 
 async function addNewUser(user: User): Promise<void> {
     try {
